21fde02c22bcf6ed18d70853f008bcca28c3a342
singh-rbir <43500177+singh-rbir@users.noreply.github.com>
2020-03-30 23:53:45 -0400

    Initial commit

---
+++ testing.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+    var div = $("#divToShowHide");
+
+    var pos = div.position(); // gets the vertical position
+
+    $(window).scroll(function(){
+        var windowpos = $(window).scrollTop();
+
+        //console.log("value of pos.top is: " + pos.top);
+        //console.log("value of windowpos is: " + windowpos);
+
+        if(windowpos >= (pos.top + 100)){
+            div.addClass("afterScroll");
+        }
+        else{
+            div.removeClass("afterScroll");
+        }
+    });
+
+    
+    var scroll = window.requestAnimationFrame ||
+            function(callback){ window.setTimeout(callback, 1000/60)};
+
+    var elementsToShow = document.querySelectorAll('.show-on-scroll'); // gets all the elements with that class into an array
+
+    function loop() {
+        elementsToShow.forEach(function (element) {
+            if (isElementInViewport(element)) {
+                element.classList.add('is-visible');
+            } else {
+                element.classList.remove('is-visible');
+            }
+        });
+
+        scroll(loop);
+    }
+        console.log("working1");
+    loop();
+
+    function isElementInViewport(el) {
+        // special bonus for those using jQuery
+        if (typeof jQuery === "function" && el instanceof jQuery) {
+            el = el[0];
+        }
+        var rect = el.getBoundingClientRect();
+        return (
+            (rect.top <= 0
+                && rect.bottom >= 0)
+                ||
+                (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+                rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+                ||
+                (rect.top >= 0 &&
+                    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+                );
+            }
+
+
+});
